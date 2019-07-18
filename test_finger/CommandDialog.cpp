2f00f64c4c3e80c462597ef62f78706c933c18ba
youDad <1632083718@qq.com>
2019-07-18 22:19:59 +0800

    等一下

--- test_finger/CommandDialog.cpp
+++ test_finger/CommandDialog.cpp
@@ -20,11 +20,37 @@ LRESULT CommandDialog::response(WPARAM w,LPARAM l){
     return TRUE;
 }
 
+class CommandControl{
+public:
+    CommandControl(int height,struct Command c,CWnd* parent){
+        CmdCode=c.CmdCode;
+        btn=new CButton();
+        btn->Create(c.Name,0,CRect(10,height,50,height+20),parent,nID);
+        for(int i=0;i<c.Type.size();i++){
+            txt.push_back(new CStatic());
+            edt.push_back(new CEdit());
+            txt[i]->Create(c.Type[i].first,0,CRect(60+60*i,height,80+60*i,height+20),parent);
+            edt[i]->Create(0,CRect(80+60*i,height,120+60*i,height+20),parent,nID*10+i);
+        }
+        nID++;
+    }
+    DataPacket Click(){
+
+    }
+private:
+    static int nID;
+    int CmdCode;
+    CButton* btn;
+    std::vector<CStatic*> txt;
+    std::vector<CEdit*> edt;
+};
+int CommandControl::nID=2001;
 // 初始化事件
 BOOL CommandDialog::OnInitDialog(){
     CDialogEx::OnInitDialog();
 
     // TODO 动态增加控件
+
     return 0;
 }
 
